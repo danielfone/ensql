9345d2b626f87ac2be2e080df889509d44607e66
Daniel Fone <daniel@fone.net.nz>
2021-02-22 15:13:34 +1300

    Set TZ = 'UTC'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ require 'support/coverage'
 
 require "ensql"
 
+ENV['TZ'] = 'UTC'
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
