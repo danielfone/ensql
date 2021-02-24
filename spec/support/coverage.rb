28467c392ac4a81b56e879ec61cb92ff62201b27
Daniel Fone <daniel@fone.net.nz>
2021-02-24 22:47:17 +1300

    Enabled coverage to be disabled

--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -1,6 +1,8 @@
-require 'simplecov'
+unless ENV["COVERAGE"] == 'false'
+  require 'simplecov'
 
-SimpleCov.start do
-  enable_coverage :branch
-  minimum_coverage line: 100, branch: 100
+  SimpleCov.start do
+    enable_coverage :branch
+    minimum_coverage line: 100, branch: 100
+  end
 end
